ba860bb37a2584801f8e77cf5c4123992e509ca1
Xijian Yan <yanxijian@163.com>
2016-03-25 18:09:43 +0800

    Add Mouse Event and Icon etx.

--- TimeTool/MainWindow.xaml.cs
+++ TimeTool/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,9 +21,13 @@ namespace TimeTool
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Forms.NotifyIcon  notifyIcon;
+        System.Windows.Forms.ContextMenu contextMenu;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitIcon();
         }
 
         private void Window_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
@@ -32,6 +38,67 @@ namespace TimeTool
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             base.DragMove();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+        }
+
+        private void InitIcon()
+        {
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.BalloonTipText = "Hello, " + Environment.UserName;
+            notifyIcon.Text = "Time Tool";
+
+            Uri iconUri = new Uri("pack://application:,,,/clock.ico");
+            Stream iconStream = Application.GetResourceStream(iconUri).Stream;
+            notifyIcon.Icon = new Icon(iconStream);
+            iconStream.Dispose();
+
+            notifyIcon.Visible = true;
+            notifyIcon.ShowBalloonTip(1000);
+
+            SetIconEvent();
+        }
+
+        private void SetIconEvent()
+        {
+            notifyIcon.MouseClick += (s, e) =>
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    if (this.Visibility == Visibility.Visible)
+                    {
+                        this.Hide();
+                    }
+                    else
+                    {
+                        this.Show();
+                    }
+                }
+                else
+                {
+                    //notifyIcon.ContextMenu.Show((Button)s, e.Location);
+                }
+            };
+        }
+
+        private void InitContexMenu()
+        {
+            contextMenu = new System.Windows.Forms.ContextMenu();
+
+            // Exit Menu Item
+            var exitMenu = new System.Windows.Forms.MenuItem();
+            exitMenu.Index = 0;
+            exitMenu.Text = "E&xit";
+            exitMenu.Click += (s, e) =>
+                {
+                    base.Close();
+                };
+
+            contextMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {exitMenu});
+            notifyIcon.ContextMenu = contextMenu;
         }
     }
 }
