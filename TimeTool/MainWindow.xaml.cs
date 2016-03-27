d4afaee551c4b3d307ba6b7cacee2c831bfd488d
Xijian Yan <yanxijian@163.com>
2016-03-27 21:17:15 +0800

    Add ContextMenu Item

--- TimeTool/MainWindow.xaml.cs
+++ TimeTool/MainWindow.xaml.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TimeTool
 {
@@ -23,6 +16,9 @@ namespace TimeTool
     {
         System.Windows.Forms.NotifyIcon  notifyIcon;
         System.Windows.Forms.ContextMenu contextMenu;
+        readonly string configFileName = "config.conf";
+        readonly string confLogTime    = "Log_Time";
+        string settingFilePath         = null;
 
         public MainWindow()
         {
@@ -48,10 +44,11 @@ namespace TimeTool
         private void InitIcon()
         {
             notifyIcon = new System.Windows.Forms.NotifyIcon();
-            notifyIcon.BalloonTipText = "Hello, " + Environment.UserName;
+            notifyIcon.BalloonTipText = "Hello, " + (Environment.UserName ?? "guy") + ".";
             notifyIcon.Text = "Time Tool";
 
-            Uri iconUri = new Uri("pack://application:,,,/clock.ico");
+            // Set the icon
+            Uri iconUri = new Uri("pack://application:,,,/Images/clock.ico");
             Stream iconStream = Application.GetResourceStream(iconUri).Stream;
             notifyIcon.Icon = new Icon(iconStream);
             iconStream.Dispose();
@@ -79,7 +76,16 @@ namespace TimeTool
                 }
                 else
                 {
-                    //notifyIcon.ContextMenu.Show((Button)s, e.Location);
+                    ContextMenu contexMenu = this.FindResource("contexMenu") as ContextMenu;
+                    contexMenu.PlacementTarget = s as Button;
+                    foreach (MenuItem item in contexMenu.Items)
+                    {
+                        if (item.Name.Equals("logTime"))
+                        {
+                            item.IsChecked = bool.Parse(GetConfig(confLogTime) ?? "False");
+                        }
+                    }
+                    contexMenu.IsOpen = true;
                 }
             };
         }
@@ -99,6 +105,93 @@ namespace TimeTool
 
             contextMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {exitMenu});
             notifyIcon.ContextMenu = contextMenu;
+        }
+
+        private void exitMenu_Click(object sender, RoutedEventArgs e)
+        {
+            base.Close();
+        }
+
+        private void logTime_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            item.IsChecked = !item.IsChecked;
+            SetConfig(confLogTime, item.IsChecked.ToString());
+        }
+
+        private string SettingFilePath()
+        {
+            if (!String.IsNullOrEmpty(settingFilePath))
+            {
+                return settingFilePath;
+            }
+
+            settingFilePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\TimeTool";
+            if (!Directory.Exists(settingFilePath))
+            {
+                Directory.CreateDirectory(settingFilePath);
+            }
+            settingFilePath += "\\" + configFileName;
+            if (!File.Exists(settingFilePath))
+            {
+                File.CreateText(settingFilePath).Close();
+            }
+            return settingFilePath;
+        }
+
+        private string GetConfig(string confName)
+        {
+            Dictionary<string, string> configs = LoadConfig();
+            string value;
+            if (configs.TryGetValue(confName, out value))
+            {
+                return value;
+            }
+            return null;
+        }
+
+        private void SetConfig(string confName, string confValue)
+        {
+            Dictionary<string, string> configs = LoadConfig();
+            string value;
+            if (configs.TryGetValue(confName, out value))
+            {
+                configs[confName] = confValue;
+            }
+            else
+            {
+                configs.Add(confName, confValue);
+            }
+
+            SaveConfig(configs);
+        }
+
+        private Dictionary<string, string> LoadConfig()
+        {
+            Dictionary<string, string> configs = new Dictionary<string, string>();
+            using (StreamReader sr = new StreamReader(SettingFilePath(), Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    configs.Add(line.Split('=')[0], line.Split('=')[1]);
+                }
+            }
+            return configs;
+        }
+
+        private void SaveConfig(Dictionary<string, string> configs)
+        {
+            FileStream fs = new FileStream(SettingFilePath(), FileMode.Truncate);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (var config in configs)
+            {
+                string line = config.Key + "=" + config.Value + Environment.NewLine;
+                sw.Write(line);
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
         }
     }
 }
